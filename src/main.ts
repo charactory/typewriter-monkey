776d5d4f0efb5a2dce4d6555c5d6aca53a57bd1b
charactory <29691+charactory@users.noreply.github.com>
2024-01-14 21:33:56 +0800

    beginnings of generate

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
 // import './style.css'
-import 'chota'
+import "chota";
+
+document.getElementById("generate_button")!.onclick = generateNoteTemplate;
+
+function getValue(name: String) {
+  return (<HTMLInputElement>(
+    document.querySelector(`input[name=${name}]:checked`)
+  ))?.value;
+}
+
+function generateNoteTemplate() {
+  const app = getValue("appearance");
+  console.log(app);
+}
