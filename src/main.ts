73a83f8d7ecccf583c1ec11bb538ddc5e283d061
charactory <29691+charactory@users.noreply.github.com>
2024-01-16 21:46:16 +0800

    other interventions

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,47 @@ function generateNoteTemplate() {
     cbt_experiment,
     cbt_reinf,
   ].filter((item) => item);
-  const cbt_string = "CBT: " + cbt.join(", ");
+
+  let cbt_string: String;
+
+  if (cbt.length > 0) {
+    cbt_string = "CBT: " + cbt.join(", ") + ". ";
+  } else {
+    cbt_string = "";
+  }
+
+  const psy_ed = [
+    getValue("psy_dep"),
+    getValue("psy_perf"),
+    getValue("psy_procras"),
+    getValue("psy_worry"),
+    getValue("psy_sleep_hyg"),
+  ].filter((item) => item);
+
+  let psy_ed_string: String;
+
+  if (psy_ed.length > 0) {
+    psy_ed_string = "Psychoeducation: " + psy_ed.join(", ") + ". ";
+  } else {
+    psy_ed_string = "";
+  }
+
+  const other_interv = [
+    getValue("motiv"),
+    getValue("relax"),
+    getValue("decision_b"),
+    getValue("assertive"),
+    getValue("val"),
+    getValue("safety_plan"),
+  ].filter((item) => item);
+
+  let other_interv_string: String;
+
+  if (other_interv.length > 0) {
+    other_interv_string = other_interv.join(", ") + ".";
+  } else {
+    other_interv_string = "";
+  }
 
   const presentation_string =
     "Appearance: " +
@@ -51,10 +91,24 @@ function generateNoteTemplate() {
     speech_vol;
 
   const risk_string = "Risk: \n";
+  const problem_string = "Presenting Problems: \n";
+  const bg_string = "Background Info: \n";
+
   const intervention_string =
-    "Intervention: \n Active listening & validation. " + cbt_string;
+    "Intervention: \nActive listening & validation. " +
+    cbt_string +
+    psy_ed_string +
+    other_interv_string;
 
   output!.value =
-    presentation_string + "\n\n" + risk_string + "\n" + intervention_string;
+    presentation_string +
+    "\n\n" +
+    risk_string +
+    "\n\n" +
+    problem_string +
+    "\n\n" +
+    bg_string +
+    "\n\n" +
+    intervention_string;
   console.log(app);
 }
