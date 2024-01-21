cc862e6e417430414b4541a505a10033a7394c85
charactory <29691+charactory@users.noreply.github.com>
2024-01-21 11:59:51 +0800

    added in my shortcuts

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,50 +1,34 @@
 $(function () {
-    $("textarea.mention").mentionsInput({
-      onDataRequest: function (mode, query, callback) {
-        var data = [
-          {
-            id: 1,
-            name: "Kenneth Auchenberg",
-            avatar: "http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif",
-            type: "contact",
-          },
-          {
-            id: 2,
-            name: "Jon Froda",
-            avatar: "http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif",
-            type: "contact",
-          },
-          {
-            id: 3,
-            name: "Anders Pollas",
-            avatar: "http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif",
-            type: "contact",
-          },
-          {
-            id: 4,
-            name: "Kasper Hulthin",
-            avatar: "http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif",
-            type: "contact",
-          },
-          {
-            id: 5,
-            name: "Andreas Haugstrup",
-            avatar: "http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif",
-            type: "contact",
-          },
-          {
-            id: 6,
-            name: "Pete Lacey",
-            avatar: "http://cdn0.4dots.com/i/customavatars/avatar7112_1.gif",
-            type: "contact",
-          },
-        ];
-  
-        data = _.filter(data, function (item) {
-          return item.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
-        });
-  
-        callback.call(this, data);
-      },
-    });
+  $("textarea.mention").mentionsInput({
+    triggerChar: "/",
+    showAvatars: false,
+    minChars: 1,
+    allowRepeat: true,
+    onDataRequest: function (mode, query, callback) {
+      var data = [
+        {
+          id: 1,
+          name: "Client reported",
+          type: "contact",
+        },
+        {
+          id: 2,
+          name: "Therapist suggested",
+          type: "contact",
+        },
+        {
+          id: 3,
+          name: "partner",
+          type: "contact",
+        },
+
+      ];
+
+      data = _.filter(data, function (item) {
+        return item.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+      });
+
+      callback.call(this, data);
+    },
   });
+});
